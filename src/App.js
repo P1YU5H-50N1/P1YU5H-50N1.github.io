8d6aec9489de7d4c502b88a14a1e8d530f9d975a
P1YU5H-50N1 <me.piyush.360@gmail.com>
2021-06-10 19:41:04 +0530

    Moved HashRouter to index.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
-import reportWebVitals from "./reportWebVitals";
-import { HashRouter, Route, Switch, Link } from "react-router-dom";
+// import reportWebVitals from "./reportWebVitals";
+import { Route, Switch, useLocation } from "react-router-dom";
 import Navbar from "./Layouts/NavBar";
 import Home from "./Layouts/Home";
 import Contact from "./Layouts/Contact";
@@ -8,19 +8,21 @@ import About from "./Layouts/About";
 import Creations from "./Layouts/Creations";
 
 function App() {
-  reportWebVitals(console.log);
+  let location = useLocation();
+
   return (
-    <HashRouter basename="/">
-      <Switch>
-        <div className="App">
-          <Navbar />
-          <Route exact path="/" component={Home} />
-          <Route exact path="/contact" component={Contact} />
-          <Route exact path="/about" component={About} />
-          <Route exact path="/creations" component={Creations} />
-        </div>
-      </Switch>
-    </HashRouter>
+    <Switch>
+      <div
+        style={{ overflow: location.pathname === "/about" ? "" : "hidden" }}
+        className="App"
+      >
+        <Navbar />
+        <Route exact path="/" component={Home} />
+        <Route exact path="/contact" component={Contact} />
+        <Route exact path="/about" component={About} />
+        <Route exact path="/creations" component={Creations} />
+      </div>
+    </Switch>
   );
 }
 
